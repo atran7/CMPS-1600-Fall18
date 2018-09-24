04b6edf6f350dcbcdf230aa50cec8cd9aca5103d
atran7 <42971053+atran7@users.noreply.github.com>
2018-09-23 23:50:25 -0500

    Update Square.java

--- Square.java
+++ Square.java
@@ -1,3 +1,69 @@
+public class Rectangle
+{
+    double length;
+    double height;
+    private static int counter = -1;
+
+    public Rectangle(double l, double h)
+    {
+        length = l;
+        height = h;
+        counter++;
+
+    }
+
+    public void setLength(double l)
+    {
+        length = l;
+    }
+
+    public void setHeight (double h)
+    {
+        height = h;
+    }
+
+    public double getHeight()
+    {
+        return height;
+    }
+
+    public double getLength()
+    {
+        return length;
+    }
+
+    public double area ()
+    {
+        return (length*height);
+    }
+
+    public double perimeter ()
+    {
+        return ((2*length) + (2*height));
+    }
+
+   public int getRectangleID()
+   {
+        return counter;
+   }
+
+   @Override
+   public String toString()
+   {
+       return "This rectangle is " + this.getHeight() + " units tall and " + this.getLength() +  " units long with a perimeter of " + this.perimeter() + " units and an area of " + this.area() + " units.";
+   }
+}
+
+
+
+
+
+
+
+
+
+
+
 public class Square extends Rectangle
 {
     public Square(double l, double h)
